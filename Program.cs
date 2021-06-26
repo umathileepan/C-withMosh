537418d1b77dee949a7f452fc56e071fff8a51bc
Uma Thileepan <77700057+umathileepan@users.noreply.github.com>
2021-06-26 16:52:22 +0100

    Primitive types

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 5;
+            int count = 20;
+            float totalPrice = 10.89f;
+            char character = 'A';
+            string firstName = "Uma";
+            bool isWorking = true;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+        }
+    }
+}
